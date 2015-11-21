7be9cf8c348808a535bd2405c30ff82c4b42cfc6
Timothy Perrett <timothy.m.perrett@one.verizon.com>
2015-11-21 07:50:50 -0800

    added release steps to travis

--- build.sh
+++ build.sh
@@ -4,16 +4,5 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   # for pull requests only do tests
   sbt ++$TRAVIS_SCALA_VERSION +test
 else
-  # for merges, do releases
-  mkdir ~/.bintray/
-  FILE=$HOME/.bintray/.credentials
-  cat <<EOF >$FILE
-realm = Bintray API Realm
-host = api.bintray.com
-user = $BINTRAY_USER
-password = $BINTRAY_API_KEY
-EOF
-  echo $BINTRAY_USER
-  echo "Created ~/.bintray/.credentials file"
   sbt "release cross with-defaults"
 fi
